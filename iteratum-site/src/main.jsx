25e2d3073dc89286c04231489b99dfadec52e28b
Jonw8192 <134105742+Jonw8192@users.noreply.github.com>
2025-09-06 21:55:34 -0400

    Update main.jsx

--- iteratum-site/src/main.jsx
+++ iteratum-site/src/main.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Site from "Site.jsx";
+import Site from "./Site.jsx";
 
+// Get the root element
+const container = document.getElementById("root");
 
-createRoot(document.getElementById("root")).render(
-<React.StrictMode>
-<Site />
-</React.StrictMode>
+// Create root and render the app
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Site />
+  </React.StrictMode>
 );
