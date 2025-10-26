c385f86cd29044300f57f871c103649f33d22ffd
Jonw8192 <134105742+Jonw8192@users.noreply.github.com>
2025-10-25 21:55:53 -0400

    Update main.jsx

--- iteratum-site/src/main.jsx
+++ iteratum-site/src/main.jsx
@@ -1,16 +1,23 @@
+// src/main.jsx
 import React from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Site from "./Site.jsx";
+import About from "./About.jsx";
+import PrivacyPolicy from "./PrivacyPolicy.jsx";
+import TermsOfService from "./TermsOfService.jsx";
+import "./index.css";
 
-// Get the root element
-const container = document.getElementById("root");
-
-// Create root and render the app
-const root = createRoot(container);
-
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Site />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Site />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/terms-of-service" element={<TermsOfService />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
