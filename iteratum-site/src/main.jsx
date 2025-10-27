875b263407fc053b7f970bd92cc048ad172e423e
Jonw8192 <134105742+Jonw8192@users.noreply.github.com>
2025-10-26 22:04:45 -0400

    Update main.jsx

--- iteratum-site/src/main.jsx
+++ iteratum-site/src/main.jsx
@@ -3,21 +3,33 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Site from "./Site.jsx";
-import About from "./About.jsx";
-import PrivacyPolicy from "./PrivacyPolicy.jsx";
-import TermsOfService from "./TermsOfService.jsx";
+import Site from "./Site";
+import About from "./About";
+import PrivacyPolicy from "./PrivacyPolicy";
+import TermsOfService from "./TermsOfService";
+
+import Resources from "./Resources";
+import Insights from "./Insights";
+import InsightTemplate from "./InsightTemplate";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Site />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/terms-of-service" element={<TermsOfService />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      {/* Marketing Site */}
+      <Route path="/" element={<Site />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+      <Route path="/terms-of-service" element={<TermsOfService />} />
+
+      {/* Resources Hub + Insights */}
+      <Route path="/resources" element={<Resources />} />
+      <Route path="/resources/insights" element={<Insights />} />
+      <Route path="/resources/insights/:slug" element={<InsightTemplate />} />
+
+      {/* Fallback to Home (optional, Netlify handles SPA via _redirects) */}
+      {/* <Route path="*" element={<Site />} /> */}
+    </Routes>
+  </BrowserRouter>
 );
